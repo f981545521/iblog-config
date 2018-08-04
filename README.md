9ece1a47617761ec4d6ceb24d3f323f3fb0fef2c
youfang <981545521@qq.com>
2018-08-04 20:15:31 +0800

    add : Spring Cloud Config 整合

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # iblog-config
 微服务配置中心
+
+### 配置步骤
+
+http://localhost:8091/iblog-config/dev
+http://localhost:8091/config-dev.properties
--- src/main/java/cn/acyou/iblogconfig/IblogConfigApplication.java
+++ src/main/java/cn/acyou/iblogconfig/IblogConfigApplication.java
@@ -2,7 +2,11 @@ package cn.acyou.iblogconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class IblogConfigApplication {
 
